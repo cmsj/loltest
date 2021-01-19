5fba8164da02d885cb9e956d0cb69be573fd09a5
Chris Jones <cmsj@tenshu.net>
2021-01-19 23:50:56 +0000

    Initial Commit

---
+++ loltest/loltest.h
@@ -0,0 +1,14 @@
+//
+//  loltest.h
+//  loltest
+//
+//  Created by Chris Jones on 19/01/2021.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface loltest : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ loltest/loltest.m
@@ -0,0 +1,16 @@
+//
+//  loltest.m
+//  loltest
+//
+//  Created by Chris Jones on 19/01/2021.
+//
+
+#import "loltest.h"
+
+@implementation loltest
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
