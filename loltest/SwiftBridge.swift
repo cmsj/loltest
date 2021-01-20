b51cbcde40c6b6d0d7ab6fa96cc61d1f4706d2f8
Chris Jones <cmsj@tenshu.net>
2021-01-20 00:45:35 +0000

    Switch NSPreferencePane subclass to Swift

--- loltest/SwiftBridge.swift
+++ loltest/SwiftBridge.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 import SwiftUI
+import PreferencePanes
+
+@objc
+class LolPrefs: NSPreferencePane {
+    @objc
+    override func loadMainView() -> NSView {
+        let view = NSHostingView(rootView: PrefsView())
+        self.mainView = view
+        self.mainView.setFrameSize(NSSize(width: 668, height: 600))
+        return view
+    }
+}
 
 @objc
 class SwiftBridge: NSObject {
